22d3562464f03ae9c8cee4a1c25a6a3e55b74cfd
kezang_ <kchoden6624@gmail.com>
2023-03-21 11:50:34 +0600

    first commit

---
+++ main task/script.js
@@ -0,0 +1,9 @@
+function myDeleteFunction() {
+    if(deleteRow = 2 ){
+        document.querySelector(".table").deleteRow(2);
+    }
+    else if(deleteRow = 3 ){
+        document.querySelector(".table").deleteRow(3);
+    }
+    
+  }
